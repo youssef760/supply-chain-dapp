0b5d685e31c0bf5f429468218b85038cfa1550d2
chigar youssef <chigarbs1git@gmail.com>
2023-12-29 22:04:01 +0100

    STARTER

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,32 +2,6 @@ const { ethers } = require('hardhat');
 const fs = require('fs');
 
 async function main() {
-  const Contract = await ethers.getContractFactory('SupplyChain');
-
-  const contract = await Contract.deploy();
-
-  await contract.deployed();
-
-  const address = JSON.stringify({ address: contract.address }, null, 4);
-
-  // Seed data after deploying the contract
-  const product1 = await contract.manufactureProduct('Product 1', 100);
-  const product2 = await contract.manufactureProduct('Product 2', 150);
-  const product3 = await contract.manufactureProduct('Product 3', 200);
-
-  console.log('Product 1 manufactured:', product1);
-  console.log('Product 2 manufactured:', product2);
-  console.log('Product 3 manufactured:', product3);
-
-  // Optionally, call other functions like packing, shipping, receiving, or selling to test the entire supply chain flow
-
-  fs.writeFile('./src/abis/contractAddress.json', address, 'utf8', (error) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    console.log('Deployed contract address: ', contract.address);
-  });
 }
 
 main().catch((error) => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,8 @@
 import React, { useEffect } from 'react';
-import Navbar from './components/Navbar';
-import MainDashboard from './components/MainDashboard';
-import { ToastContainer } from 'react-toastify';
-import { isWalletConnected, loadProductDetails } from './SupplyChainContract';
-import { useDispatch, useSelector } from 'react-redux';
-import { store } from './store';
-import { globalActions } from './store/globalSlices';
-import ProductDetailsTable from './components/ProductDetailsTable';
 
 
-function App() {
-  const dispatch = useDispatch();
-  const { productsDetails } = useSelector((states) => states.globalStates);
-  const { setProductsDetails } = globalActions;
 
-  useEffect(() => {
-    const loadBlockchain = async () => {
-      await isWalletConnected();
-      await loadProductDetails();
-      console.log('Blockchain Loaded');
-    };
-
-    loadBlockchain();
-  }, [dispatch]);
-
-  return (
-    <div className="font-sans">
-      <Navbar />
-      <MainDashboard />
-      <ProductDetailsTable products={productsDetails} />
-      <ToastContainer
-        position="bottom-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={true}
-        closeOnClick
-        rtl={false}
-        pauseOnHover
-        pauseOnFocusLoss
-        draggable
-        theme="dark"
-      />
-    </div>
-  );
-}
+function App() {}
 
 
 
